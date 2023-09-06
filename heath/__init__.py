cfbfb80cdaecb6e871e65da0674c437954f4f402
Jonatan Skogsfors <jonatan@skogsfors.net>
2023-09-06 16:59:36 +0200

    0.1.5
    Also:
    - Reuse version string

--- heath/__init__.py
+++ heath/__init__.py
@@ -1,1 +1,6 @@
-__version__ = "0.1.4"
+import tomllib
+from pathlib import Path
+
+pyproject_path = Path(__file__).parent.parent / "pyproject.toml"
+pyproject = tomllib.loads(pyproject_path.read_text())
+__version__ = pyproject["tool"]["poetry"]["version"]
