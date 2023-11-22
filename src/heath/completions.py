cb2d06b4bac0b6cd2929d997e176f739f818aea5
Jonatan Skogsfors <jonatan.skogsfors@smhi.se>
2023-11-22 11:24:03 +0100

    Case insesitive completion

--- src/heath/completions.py
+++ src/heath/completions.py
@@ -4,14 +4,14 @@ from pathlib import Path
 from heath.project import Project
 
 
-def complete_projects(ctx, param, incomplete, all_day=False):
+def complete_projects(ctx, param, incomplete: str, all_day: bool = False):
     if heath_folder := os.environ.get("HEATH_FOLDER"):
         projects_file = Path(heath_folder) / "projects.cfg"
         projects = Project.from_configuration_path(projects_file)
         return [
             project.key
             for project in projects
-            if project.key.startswith(incomplete) and project.all_day == all_day
+            if project.key.lower().startswith(incomplete.lower()) and project.all_day == all_day
         ]
     return []
 
